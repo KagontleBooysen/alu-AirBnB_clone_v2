655059447d13ab4ec1b0c727e34a6f744b0e71a5
KagontleBooysen <k.booysen@alustudent.com>
2023-03-10 17:47:32 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,13 +30,13 @@ def do_deploy(archive_path):
         tmp_path = "/tmp/{}".format(name)
 
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        run("sudo mkdir -p {}".format(releases_path))
+        run("sudo tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("sudo rm {}".format(tmp_path))
+        run("sudo mv {}web_static/* {}".format(releases_path, releases_path))
+        run("sudo rm -rf {}web_static".format(releases_path))
+        run(" sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
     except:
