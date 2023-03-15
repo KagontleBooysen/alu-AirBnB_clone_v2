aace3061203b425fbdee903b63a49bf9ce4c4c5d
KagontleBooysen <k.booysen@alustudent.com>
2023-03-15 17:15:50 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import local, env, put, run
 from datetime import datetime
 import os
 
-env.hosts = ['54.167.15.9', '54.90.243.170']
+env.hosts = ['54.167.15.9', '54.162.155.171']
 env.user = 'ubuntu'
 
 
@@ -26,23 +26,26 @@ def do_deploy(archive_path):
     """distributes an archive to a web servers"""
 
     if not os.path.exists(archive_path):
-
         return False
     try:
         put(archive_path, "/tmp/")
         filename = archive_path.split("/")[-1]
         name = filename.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name), timeout=10)
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            filename, name))
-        run("sudo rm /tmp/{}".format(filename))
-        run("sudo cp -rf /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
+            filename, name), timeout=10)
+        run("rm /tmp/{}".format(filename), timeout=10)
+        run("cp -rf /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name), timeout=10)
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name), timeout=10)
+        run("rm -rf /data/web_static/current", timeout=10)
+        run("ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(
-            name))
+            name), timeout=10)
         return True
-    except Exception:
+    except FileNotFoundError:
         return False
+    except Exception as e:
+        print(e)
+        return False
+
