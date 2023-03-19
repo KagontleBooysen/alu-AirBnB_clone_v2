9ed70ab7bca78532ad38c0c33d4750e746fe961c
KagontleBooysen <k.booysen@alustudent.com>
2023-03-19 20:56:40 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""List all states Module"""
-
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list')
-def states_list():
-    """display a HTML the States"""
-    all_states = list(storage.all(State).values())
-    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """function that call close methofd"""
+def handle_teardow(self):
+    """
+    Después de cada solicitud, debe eliminar
+    la sesión actual de SQLAlchemy
+    """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states_list', strict_slashes=False)
+def list_of_state():
+    """
+    Función llamada con la ruta /states_list
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
