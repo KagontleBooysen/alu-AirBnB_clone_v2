a8bd5ee1ac2b2fed445fee712f31f013532d2632
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 07:02:43 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
 """
-Starts a Flask web application:
-Update FileStorage: (models/engine/file_storage.py)
-
-Add a public method def close(self):: call reload() method for deserializing the JSON file to objects
-Update DBStorage: (models/engine/db_storage.py)
-
-Add a public method def close(self):: call remove() method on the private session attribute (self.__session) tips or close() on the class Session tips
-Update State: (models/state.py) - If it’s not already present
-
-If your storage engine is not DBStorage, add a public getter method cities to return the list of City objects from storage linked to the current State
-"""
-
 from models import storage
-from models.state import State
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """Comment"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy Session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
