64f4342a14b4296ec3104a9d8d870ae76596896f
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 04:41:17 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,68 +1,27 @@
 #!/usr/bin/python3
-"""This module uses Flask and starts a web application"""
 
+"""Starts a Flask web application"""
 
-from models import *
-from flask import Flask, render_template
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Hello world for flask"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """display HBNB for /hbnb"""
-    return "HBNB"
-
-
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_route(text):
-    """display `C` followed by value of text variable
-    replace underscores with a space"""
-    return "C {:s}".format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def py_route(text="is cool"):
-    """display `Python` followed by value of text variable
-    replace underscores with a space"""
-    return "Python {:s}".format(text.replace("_", " "))
-
-
-@app.route('/number/<int:num>', strict_slashes=False)
-def number_route(num):
-    """display `n is a number` only if `n` is an int"""
-    return "{:d} is a number".format(num)
-
-
-@app.route('/number_template/<int:num>', strict_slashes=False)
-def number_template(num):
-    """displays a HTML page if `num` is an integer"""
-    return render_template('5-number.html', num=num)
-
-
-@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
-def number_odd_or_even(num):
-    """displays a HTML page if `num` is an integer
-    message on page changes depending on value passed"""
-    return render_template('6-number_odd_or_even.html', num=num)
-
-
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """displays a HTML page of state objects present in DBstorage
-    message on page changes depending on value passed"""
-    return render_template('7-states_list.html', states=storage.all("State"))
+def states():
+    """Comment"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def teardown(err):
+def teardown(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
