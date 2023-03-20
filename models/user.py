e716e91521757e63f591890f90bc04935a27c8b6
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 04:06:45 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
-s module defines a class User"""
+#!/usr/bin/python3
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
