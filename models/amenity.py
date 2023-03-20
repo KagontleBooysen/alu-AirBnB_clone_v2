d847d4432969bf5d75bf3ebf3f44990ace1d7b67
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 03:58:02 +0000

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,4 +1,5 @@
-tate Module for HBNB project """
+#!/usr/bin/python3
+"""State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
