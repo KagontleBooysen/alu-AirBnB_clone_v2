25debe8a137c7d209591ca1cc953941ef57a5b52
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 20:35:16 +0000

    state.py

--- models/state.py
+++ models/state.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
-from os import getenv
-from models.base_model import BaseModel
-from models.city import City, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,  Integer, String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    # Added for task 6
-    # if getenv("HBNB_TYPE_STORAGE") == "db":
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state', cascade="all, delete")
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
-        # Added for task 6
-        # if getenv("HBNB_TYPE_STORAGE") == "file":
         @property
         def cities(self):
-            list_of_cities = []
-            dic_cities = models.storage.all(City)
-            # for city in dic_cities.items():
-            for city in dic_cities.values():
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
                 if city.state_id == self.id:
-                    list_of_cities.append(city)
-            return list_of_cities
+                    related_cities.append(city)
+            return related_cities
