a2e03022d856dcfe95e3296fe1d49a4fbb611e45
KagontleBooysen <k.booysen@alustudent.com>
2023-03-20 06:18:09 +0000

    state.py

--- models/state.py
+++ models/state.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
-"""module for HBNB project """
-from models.base_model import BaseModel, Base
-
-# Define storage_type variable
-storage_type = getenv('HBNB_TYPE_STORAGE')
-
-from models.city import City
-from sqlalchemy import Column, String
+"""This is the state class"""
+import models
+from os import getenv
+from models.base_model import BaseModel
+from models.city import City, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,  Integer, String
 from sqlalchemy.orm import relationship
 
-class State(BaseModel, Base):
-    """ State class / table model"""
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
 
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    # Added for task 6
+    # if getenv("HBNB_TYPE_STORAGE") == "db":
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state', cascade="all, delete")
+    else:
+        name = ""
+
+        # Added for task 6
+        # if getenv("HBNB_TYPE_STORAGE") == "file":
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
+            list_of_cities = []
+            dic_cities = models.storage.all(City)
+            # for city in dic_cities.items():
+            for city in dic_cities.values():
                 if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+                    list_of_cities.append(city)
+            return list_of_cities
