af19b4b2e9dff51ec5505dfb735c68d5f23f8c2a
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 20:50:43 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application:
--Your web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
--To load all cities of a State:
-     If your storage engine is DBStorage, you must use cities relationship
-     Otherwise, use the public getter method cities
--After each request you must remove the current SQLAlchemy Session:
-       Declare a method to handle @app.teardown_appcontext
-       Call in this method storage.close()
--Routes:
-       /cities_by_states: display a HTML page: (inside the tag BODY)
-            -H1 tag: “States”
-             -UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-                   -LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z)
-         -LI tag: description of one City: <city.id>: <B><city.name></B>
--Import this 7-dump to have some data
--You must use the option strict_slashes=False in your route definition
+"""Start web application with two routings
 """
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states')
 def states_list():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda x: x.name)
-    return render_template('states_list.html', states=sorted_states)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
