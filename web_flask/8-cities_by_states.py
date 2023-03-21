cc65edf3a76fa778722177ae5a15459e9070a6c3
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 18:58:52 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,20 +2,21 @@
 """
 starts a Flask web application:
 -Your web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
+-To load all cities of a State:
+     If your storage engine is DBStorage, you must use cities relationship
+     Otherwise, use the public getter method cities
+-After each request you must remove the current SQLAlchemy Session:
+       Declare a method to handle @app.teardown_appcontext
+       Call in this method storage.close()
 -Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of
-         the text variable (replace underscore _ symbols with a space )
-        /python/(<text>): display “Python ”, followed by the value of the text
-        variable (replace underscore _ symbols with a space )
-- The default value of text is “is cool”
-        /number/<n>: display “n is a number” only if n is an integer
-        /number_template/<n>: display a HTML page only if n is an integer:
-- H1 tag: “Number: n” inside the tag BODY
-        /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-- H1 tag: “Number: n is even|odd” inside the tag BODY
-You must use the option strict_slashes=False in your route definition
+       /cities_by_states: display a HTML page: (inside the tag BODY)
+             H1 tag: “States”
+             UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+                   LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z)
+         LI tag: description of one City: <city.id>: <B><city.name></B>
+-Import this 7-dump to have some data
+-You must use the option strict_slashes=False in your route definition
 """
 from models import storage
 from flask import Flask
