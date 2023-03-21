f468748577330422cfeea64af8a1cbb14d96b856
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 15:41:55 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+-starts a Flask web application:
 -Your web application must be listening on 0.0.0.0, port 5000
 -You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
 -After each request you must remove the current SQLAlchemy Session:
