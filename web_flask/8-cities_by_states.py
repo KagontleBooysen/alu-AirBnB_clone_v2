f20d2f38aa0c43c8a8f00b7e99148f6158bc88f1
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 09:25:24 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-"""This module uses Flask and starts a web application"""
+"""
+starts a Flask web application:
+-Your web application must be listening on 0.0.0.0, port 5000
+-You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
+-After each request you must remove the current SQLAlchemy Session:
+          Declare a method to handle @app.teardown_appcontext
+          Call in this method storage.close()
+-Routes:
+        /states_list: display a HTML page: (inside the tag BODY)
+-H1 tag: “States”
+-UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+-LI tag: description of one State: <state.id>: <B><state.name></B>
+Import this 7-dump to have some data
+You must use the option strict_slashes=False in your route definition
+"""
 
-
-from models import *
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -74,4 +87,4 @@ def teardown(err):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=None)
