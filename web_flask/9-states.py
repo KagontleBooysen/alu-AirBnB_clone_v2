da1fb6a18293f332a323d7081356527578a829df
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 12:18:15 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,27 +1,8 @@
-#!/usr/bin/python3
-"""
-Starts a Flask web application:
--Your web application must be listening on 0.0.0.0, port 5000
--use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
--To load all cities of a State:
-   If your storage engine is DBStorage, you must use cities relationship
-   Otherwise, use the public getter method cities
-
--Routes:
-       /states: display a HTML page: (inside the tag BODY)
-         -H1 tag: “States”
-         -UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-             -LI tag: description of one State: <state.id>: <B><state.name></B>
-      /states/<id>: display a HTML page: (inside the tag BODY)
-      - If a State object is found with this id:
-       - H1 tag: “State: ”
-        -H3 tag: “Cities:”
-       - UL tag: with the list of City objects linked to the State sorted by name (A->Z)
-        -LI tag: description of one City: <city.id>: <B><city.name></B
-Otherwse:
-        -H1 tag: “Not found!”
-You must use the option strict_slashes=False in your route definition
-Import this 7-dump to have some data
+ a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
 """
 from models import storage
 from flask import Flask
@@ -33,7 +14,6 @@ app = Flask(__name__)
 @app.route("/states", strict_slashes=False)
 def states():
     """Displays an HTML page with a list of all States.
-
     States are sorted by name.
     """
     states = storage.all("State")
@@ -56,4 +36,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
