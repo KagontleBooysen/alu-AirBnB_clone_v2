0291bb24dc43f0e80ecd0fdbdbf2dbec7ee25e03
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 19:41:58 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,25 +19,25 @@ You must use storage for fetching data from the storage engine (FileStorage or D
 -You must use the option strict_slashes=False in your route definition
 """
 
-from flask import render_template
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template('states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_session(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
