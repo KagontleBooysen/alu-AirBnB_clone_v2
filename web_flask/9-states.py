f91151de1766d90196460bfc7f696184e5657e9d
KagontleBooysen <k.booysen@alustudent.com>
2023-03-21 12:05:33 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Starts a Flask web application:
 
-The application listens on 0.0.0.0, port 5000.
+-The application listens on 0.0.0.0, port 5000.
+-use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
+-To load all cities of a State:
+   If your storage engine is DBStorage, you must use cities relationship
+   Otherwise, use the public getter method cities
+
 Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+       /states: display a HTML page: (inside the tag BODY)
+         -H1 tag: “States”
+         -UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+             -LI tag: description of one State: <state.id>: <B><state.name></B>
+      /states/<id>: display a HTML page: (inside the tag BODY)
+       If a State object is found with this id:
+        H1 tag: “State: ”
+        H3 tag: “Cities:”
+        UL tag: with the list of City objects linked to the State sorted by name (A->Z)
+        LI tag: description of one City: <city.id>: <B><city.name></B
+Otherwise:
+        H1 tag: “Not found!”
+You must use the option strict_slashes=False in your route definition
+Import this 7-dump to have some data
 """
 from models import storage
 from flask import Flask
