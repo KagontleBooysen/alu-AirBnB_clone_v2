d211349d2ff77c6c9fcee873b1b365a6df3f37ca
KagontleBooysen <k.booysen@alustudent.com>
2023-03-22 04:41:50 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,31 +2,37 @@
 """Start web application with two routings
 """
 
-from models import storage
-from models.state import State
-from flask import Flask, render_template
+from flask import Flask
+from models import storage, State
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
-
-
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(exceptions):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    state_obj = storage.all("State")
+    city_obj = storage.all("City")
+    states = list()
+    cities = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    for city, value in city_obj.items():
+        cities.append(value)
+    return render_template("8-cities_by_states.html",
+                           states=states,
+                           cities=cities)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)e
