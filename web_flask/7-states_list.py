bf1ac3e6094b53cc55ab136b51ffdfa01367c91d
KagontleBooysen <k.booysen@alustudent.com>
2023-03-22 17:50:27 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
+"""starts a Flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+# from models.engine.db_storage import close
 
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Displays a HTML page with a list of states """
+    """ states = storage.all('State').values()"""
+    states = sorted(list(storage.all(State).values()),
+                    key=lambda state: state.name)
+    # print(states)
+    return render_template('7-states_list.html', states=states)
+
+# Close the session after each request
+
+
 @app.teardown_appcontext
-def tear_down(self):
-    """tear down app context"""
+def close_session(exception):
+    """ Closes the session after each request """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """lists states from database
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('7-states_list.html', all_states=all_states)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
