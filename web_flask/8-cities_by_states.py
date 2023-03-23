32353813ca9a5964897d85242d436a260f71a6d3
KagontleBooysen <k.booysen@alustudent.com>
2023-03-23 18:58:51 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,26 +3,26 @@
 Returns:
     _type_: _description_
 """
-
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    """loads all cities of a State"""
+
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """closes the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
