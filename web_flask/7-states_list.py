89a32d477d8a49a5fbfcd3f1604d61b4c08eae35
KagontleBooysen <k.booysen@alustudent.com>
2023-03-23 19:32:54 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,7 @@ Returns:
 
 from flask import Flask, render_template
 
-app = Flask(__name_)
+app = Flask(__name__)
 
 @app.teardown_appcontext
 def handle_teardow(self):
@@ -28,3 +28,6 @@ def list_of_state():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+
+
