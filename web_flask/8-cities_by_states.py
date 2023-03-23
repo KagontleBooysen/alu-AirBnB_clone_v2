1ca557a4adf8abb9ebf26b39ba6f21491941f125
KagontleBooysen <k.booysen@alustudent.com>
2023-03-23 17:11:24 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage, State
-from flask import render_template
+
+"""Script that starts a Flask web apllication"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """loads all cities of a State"""
+
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
 @app.teardown_appcontext
-def teardown(exceptions):
+def teardown(self):
+    """closes the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-    return render_template("8-cities_by_states.html",
-                           states=states,
-                           cities=cities)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)e
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
