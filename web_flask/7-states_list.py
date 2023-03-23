1913dbdbf301901ac29c68af81fddd96f45a6c14
KagontleBooysen <k.booysen@alustudent.com>
2023-03-23 17:09:05 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
-"""Starts  a Flask web application"""
+"""Script that starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """returns list of states"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def teardown(self):
+    """closes the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
