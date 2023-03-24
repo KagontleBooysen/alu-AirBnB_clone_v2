a0e63b2272bf27211e31404ed3ca5bad70ea9da2
KagontleBooysen <k.booysen@alustudent.com>
2023-03-24 15:33:45 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,28 +3,30 @@
 Returns:
     _type_: _description_
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ display a HTML page """
-    # return a list of State object.
-    states = list(storage.all(State).values())
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+@app.route('/states_list',  strict_slashes=False)
+def display_states_list():
+    """ retrieve list of states and passes to template"""
+    # states_list = storage.all("State")
+    states_list = []
+    dic_states = storage.all(State)
+    for state in dic_states.values():
+        # for city in dic_cities.values():
+        states_list.append(state)
+    return render_template('7-states_list.html', states=states_list)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """ remove the current SQLAlchemy Session """
+def teardown_db(exception):
+    '''close connections'''
     storage.close()
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run()
