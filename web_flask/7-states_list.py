e58fe0d14f70b6ab4a597e03cd77a26869cdaa2a
KagontleBooysen <k.booysen@alustudent.com>
2023-03-24 15:07:04 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web application and do some dummy stuff
+""" A flask server to return id and name of a State object
 Returns:
     _type_: _description_
 """
-
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ display a HTML page """
+    # return a list of State object.
+    states = list(storage.all(State).values())
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
-def handle_teardow(self):
-    """
-    Después de cada solicitud, debe eliminar
-    la sesión actual de SQLAlchemy
-    """
+def teardown(exception):
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_of_state():
-    """
-    Función llamada con la ruta /states_list
-    """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
