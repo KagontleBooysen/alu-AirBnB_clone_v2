cc789988d3bef2afdea3489bd37da3942cd0c0e6
KagontleBooysen <k.booysen@alustudent.com>
2023-03-24 15:50:49 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,22 +11,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list',  strict_slashes=False)
-def display_states_list():
-    """ retrieve list of states and passes to template"""
-    # states_list = storage.all("State")
-    states_list = []
-    dic_states = storage.all(State)
-    for state in dic_states.values():
-        # for city in dic_cities.values():
-        states_list.append(state)
-    return render_template('7-states_list.html', states=states_list)
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    '''close connections'''
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
