1e1f04821e88004af4d64a4e60988d002bb4a547
KagontleBooysen <k.booysen@alustudent.com>
2023-03-24 18:39:56 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web application and do some dummy stuff
-Returns:
-    _type_: _description_
 """
+Write a script that starts a Flask web application: Your web application must
+be listening on 0.0.0.0,
+port 5000.
+You must use storage for fetching data from the storage engine (FileStorage or
+DBStorage) => from models
+import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+    - Declare a method to handle @app.teardown_appcontext
+    - Call in this method storage.close()
+strict_slashes=False in your route definition
+"""
+
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """ Displays a HTML page with a list of states """
-    states = sorted(list(storage.all(State).values()),
-                    key=lambda state: state.name)
-    return render_template('9-states.html', states=states)
+def states_list():
+    """
+    display a HTML page
+    """
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
-@app.route('/states/<state_id>', strict_slashes=False)
-def states_id(state_id):
-    """ Displays a HTML page with a list of states """
-    states = sorted(list(storage.all(State).values()),
-                    key=lambda state: state.name)
-    state_found = False
-    for state in states:
-        if state.id == state_id:
-            state_found = True
-            return render_template(
-                '9-states.html',
-                states=states,
-                cities=state.cities,
-                id=state_id)
-    if not state_found:
-        return render_template(
-            '9-states.html',
-            states=states,
-            id=None)
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """display a HTML page”
+    """
+    state_list = storage.all('State').values()
+    for state in state_list:
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html', state=None)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """ Closes the session after each request """
+def teardown(self):
+    """remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
