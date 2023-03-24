0d6133395e7b1356fa2f2eadc639d453ebab1c43
KagontleBooysen <k.booysen@alustudent.com>
2023-03-24 14:57:53 +0000

    state.py

--- models/state.py
+++ models/state.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-import models
-from sqlalchemy.orm import relationship
+# import sqlalchemy modules
 from sqlalchemy import Column, String
-from models.city import City
-from os import getenv
-
-storage = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.orderinglist import ordering_list
 
 
 class State(BaseModel, Base):
-    """ State class """
+
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-    if storage == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref='states')
-    else:
-        name = ""
-    if storage != 'db':
-        @property
-        def cities(self):
-            """
-            return list of cities
-            """
-            list_cities = []
-            cities = models.storage.all(City)
-            for key, city_obj in cities.items():
-                if city_obj.state_id == self.id:
-                    list_cities.append(city_obj)
-            return list_cities
+    # write a code to make BaseModel column order first in the table
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+    # write a code to make BaseModel column order first in the table
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of City objects
+        from storage linked to the current State"""
+        from models import storage
+        from models.city import City
+        cities = []
+        for key, value in storage.all(City).items():
+            if value.state_id == self.id:
+                cities.append(value)
+        return cities
